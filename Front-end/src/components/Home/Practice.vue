4bce9f0f0a8dae40efcf00b7eed51a6efab58737
HongJae Eom <ox1351@naver.com>
2020-09-16 17:17:52 +0900

    [Feat(F)] 폴더구조, Home, Nav 제작

---
+++ Front-end/src/components/Home/Practice.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1>실습 과정</h1>
+    <img src="" alt="">
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -1,6 +1,8 @@
+// import { LogicalNot } from "@tensorflow/tfjs";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/account/Login.vue"
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   }
 ];
 
