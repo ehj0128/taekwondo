aaa80bcafc697410595b1bf697969e73c57a38c7
Eom Hongjae <ox1351@naver.com>
2020-09-17 22:25:15 +0900

    [Feat(f)] HomePage구상

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-container style="width: 63rem;">
-    <VideoClass/>
-    <Practice />
-  </v-container>
+  <div>
+    <video loop="loop" autoplay playsinline muted preload="none" src="https://mediadistribution.espn.com/wp-content/themes/Divi-MD/assets/videos/media_distribution_trim.mp4" style="width:100%"></video>
+    <!-- <VideoClass/>
+    <Practice /> -->
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Practice from "../components/Home/Practice"
-import VideoClass from "../components/Home/VideoClass"
+// import Practice from "../components/Home/Practice"
+// import VideoClass from "../components/Home/VideoClass"
 
 export default {
   name: "Home",
-  components: {
-    Practice,
-    VideoClass
-  }
+  // components: {
+  //   Practice,
+  //   VideoClass
+  // }
 };
 </script>
