ce14447fa8bcbeedc0e558d20fc2169796627165
SangEun <eight34@naver.com>
2020-09-21 22:07:14 +0900

    commit for checkout

--- Front-end/src/views/Introduction.vue
+++ Front-end/src/views/Introduction.vue
@@ -1,77 +1,48 @@
 <template>
   <div>
     <div>
-  <v-item-group mandatory active-class="primary">
-    <v-container>
-      <v-row>
-        <v-col
-          v-for="(menu) in menus"
-          :key="menu"
-        >
-          <v-item v-slot:default="{ active, toggle }">
-            <v-card
-              class="d-flex align-center"
-              dark
-              height="200"
-              @click="toggle"
-            >
-              <v-scroll-y-transition>
-                <div
-                  v-if="active"
-                  class="display-3 flex-grow-1 text-center"
-                >
-                  {{ menu }}
-                </div>
-              </v-scroll-y-transition>
-            </v-card>
-          </v-item>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-item-group>
+      <v-item-group mandatory active-class="primary">
+          <v-row class="justify-center" style="background-color:rgb(249, 249, 250)">
+            <v-col
+              v-for="(menu, i) in menus"
+              :key="menu"
+              cols="1"
+              >
+              <v-item v-slot:default="{ toggle, active }">
+                <v-card class="d-flex align-center" color="rgb(249, 249, 250)" flat height="50px" width="50px" @click="toggle; window=i;">
+                  <v-scroll-y-transition>
+                    <div v-if="active" class="flex-grow-1 text-center">
+                      <v-img height="50px" width="50px" :src="menu.img" alt></v-img>  
+                    </div>
+                    <div v-else class="flex-grow-1 text-center">
+                        <v-img height="50px" width="50px" :src="menu.img" alt></v-img>  
+                    </div>
+                  </v-scroll-y-transition>
+                </v-card>
+              </v-item>
+              <div>{{ menu.title }}</div> 
+            </v-col>
+          </v-row>
+      </v-item-group>
     </div>    
 
-
-
-
-
-
-
     <v-container style="width: 63rem;">
-      <v-row>
-        <v-col cols="3" style="background-color: red">
-            <v-card class="mx-auto" max-width="500">
-            <v-list>
-              <v-list-item-group v-model="model" mandatory>
-                <v-list-item v-for="(item, i) in menus" :key="item">
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item" @click="window=i"></v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
+      <v-window v-model="window" class="elevation-1">
+        <v-window-item v-for="n in length" :key="n">
+          <v-card flat>
+            <v-card-text>
+              <v-row class="mb-4" align="center">
+                <strong class="title">Title {{ n }}</strong>
+                <v-spacer></v-spacer>
+              </v-row>
+              <div v-if="window==0"><Intro /></div>
+              <div v-else-if="window==1"><History /></div>
+              <div v-else-if="window==2"><Category /></div>
+              <div v-else><Technique /></div>
+            </v-card-text>
           </v-card>
-        </v-col>
-
-        <v-col cols="9" style="background-color: blue">
-          <v-window v-model="window" class="elevation-1">
-            <v-window-item v-for="n in length" :key="n">
-              <v-card flat>
-                <v-card-text>
-                  <v-row class="mb-4" align="center">
-                    <strong class="title">Title {{ n }}</strong>
-                    <v-spacer></v-spacer>
-                  </v-row>
-                  <div v-if="window==0"><Intro /></div>
-                  <div v-else-if="window==1"><History /></div>
-                  <div v-else-if="window==2"><Category /></div>
-                  <div v-else><Technique /></div>
-                </v-card-text>
-              </v-card>
-            </v-window-item>
-          </v-window>
-        </v-col>
-      </v-row>
+        </v-window-item>
+      </v-window>
     </v-container>
   </div>
 </template>
@@ -92,7 +63,24 @@ export default {
   },
   data(){
     return {
-      menus : ["소개", "역사", "기술 체계", "용어 사전"],
+      menus : [
+        { 
+          title: "소개",
+          img: "/class-img/img-class1.png"
+        },
+        { 
+          title: "역사",
+          img: "/class-img/img-class2.png"
+        },
+        { 
+          title: "기술 체계",
+          img: "/class-img/img-class3.png"
+        },
+        { 
+          title: "용어 사전",
+          img: "/class-img/img-class4.png"
+        },
+      ],
       model: 0,
       length: 4,
       window: 0,
