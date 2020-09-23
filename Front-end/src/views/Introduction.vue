882a2e5b2826899d06db40c0d871cb9ac918ef00
username <vrladlsghv@naver.com>
2020-09-23 21:49:39 +0900

    [Feat(F)] 태권도 소개문서 Closes S03P23B309-33

--- Front-end/src/views/Introduction.vue
+++ Front-end/src/views/Introduction.vue
@@ -2,36 +2,57 @@
   <div>
     <div>
       <v-item-group mandatory active-class="primary">
-          <v-row class="justify-center" style="background-color:rgb(245, 245, 247, 0.7)">
-            <v-btn-toggle v-model="toggle_exclusive" mandatory>
-              <v-col class="text-center" v-for="(menu, i) in menus" :key="menu.title" style="background-color:rgb(245, 245, 247, 0.7)">
-                <v-btn active-class="no-active" text @click="window=i" style="outline:none">
-                  <v-img v-if="window==i" height="50px" width="50px" :src="menu.imgPath2"></v-img>
-                  <v-img v-else height="50px" width="50px" :src="menu.imgPath"></v-img>
-                </v-btn>
-                <div v-if="window==i" style="color:rgb(0, 112, 201)" class="text-caption">{{ menu.title }}</div>
-                <div v-else class="text-caption">{{ menu.title }}</div>
-              </v-col>
-            </v-btn-toggle>
-          </v-row>
+        <v-row
+          class="justify-center"
+          style="background-color:rgb(245, 245, 247, 0.7)"
+        >
+          <v-btn-toggle v-model="toggle_exclusive" mandatory>
+            <v-col
+              class="text-center"
+              v-for="(menu, i) in menus"
+              :key="menu.title"
+              style="background-color:rgb(245, 245, 247, 0.7)"
+            >
+              <v-btn
+                active-class="no-active"
+                text
+                @click="Window = i"
+                style="outline:none"
+              >
+                <v-img
+                  v-if="Window == i"
+                  height="50px"
+                  width="50px"
+                  :src="menu.imgPath2"
+                ></v-img>
+                <v-img
+                  v-else
+                  height="50px"
+                  width="50px"
+                  :src="menu.imgPath"
+                ></v-img>
+              </v-btn>
+              <div
+                v-if="Window == i"
+                style="color:rgb(0, 112, 201)"
+                class="text-caption"
+              >
+                {{ menu.title }}
+              </div>
+              <div v-else class="text-caption">{{ menu.title }}</div>
+            </v-col>
+          </v-btn-toggle>
+        </v-row>
       </v-item-group>
-    </div>    
+    </div>
 
     <v-container style="width: 63rem;">
-      <v-window v-model="window" class="elevation-1">
+      <v-window v-model="Window">
         <v-window-item v-for="n in length" :key="n">
-          <v-card flat>
-            <v-card-text>
-              <v-row class="mb-4" align="center">
-                <strong class="title">Title {{ n }}</strong>
-                <v-spacer></v-spacer>
-              </v-row>
-              <div v-if="window==0"><Intro /></div>
-              <div v-else-if="window==1"><History /></div>
-              <div v-else-if="window==2"><Category /></div>
-              <div v-else><Technique /></div>
-            </v-card-text>
-          </v-card>
+          <Intro v-show="Window == 0" />
+          <History v-show="Window == 1" />
+          <Category v-show="Window == 2" />
+          <Technique v-show="Window == 3" />
         </v-window-item>
       </v-window>
     </v-container>
@@ -39,55 +60,54 @@
 </template>
 
 <script>
-import Intro from '../components/Introduction/Intro.vue'
-import History from '../components/Introduction/History.vue'
-import Category from '../components/Introduction/Category.vue'
-import Technique from '../components/Introduction/Technique.vue'
+import Intro from "../components/Introduction/Intro.vue";
+import History from "../components/Introduction/History.vue";
+import Category from "../components/Introduction/Category.vue";
+import Technique from "../components/Introduction/Technique.vue";
 
 export default {
   name: "introduction",
-  components:{
+  components: {
     Intro,
     History,
     Category,
-    Technique
+    Technique,
   },
-  data(){
+  data() {
     return {
-      menus : [
-        { 
+      menus: [
+        {
           title: "소개",
           imgPath: require("./../assets/class-img/img-class1.png"),
-          imgPath2: require("./../assets/class-img/img-class1-a.png")
+          imgPath2: require("./../assets/class-img/img-class1-a.png"),
         },
-        { 
+        {
           title: "역사",
           imgPath: require("./../assets/class-img/img-class2.png"),
-          imgPath2: require("./../assets/class-img/img-class2-a.png")
+          imgPath2: require("./../assets/class-img/img-class2-a.png"),
         },
-        { 
+        {
           title: "기술 체계",
           imgPath: require("./../assets/class-img/img-class3.png"),
-          imgPath2: require("./../assets/class-img/img-class3-a.png")
+          imgPath2: require("./../assets/class-img/img-class3-a.png"),
         },
-        { 
+        {
           title: "용어 사전",
           imgPath: require("./../assets/class-img/img-class4.png"),
-          imgPath2: require("./../assets/class-img/img-class4-a.png")
+          imgPath2: require("./../assets/class-img/img-class4-a.png"),
         },
       ],
       model: 0,
       length: 4,
-      window: 0,
+      Window: 0,
       toggle_exclusive: undefined,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style scoped>
-.v-btn--active.no-active::before {                                                                             
+.v-btn--active.no-active::before {
   background-color: transparent !important;
 }
-
 </style>
