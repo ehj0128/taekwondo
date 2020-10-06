d2f21da40e19c6f5b7ff9e2be2d20edcf90174c6
Kanggyu.Min <macaron61@gmail.com>
2020-10-06 18:30:16 +0900

    [Fix(F)] 로그인 화면 삭제

--- Front-end/src/components/Nav.vue
+++ Front-end/src/components/Nav.vue
@@ -2,6 +2,7 @@
   <v-app-bar dense app color="rgb(51,51,51)" dark>
     <v-container style="width: 63rem;">
       <v-row>
+        <v-spacer></v-spacer>
         <v-btn cols=1 text @click="$router.push({name: 'Home'})">로고</v-btn>
         <v-spacer></v-spacer>
         <v-btn cols=1 text @click="$router.push({name: 'Introduction'})">태권도란</v-btn>
@@ -10,7 +11,6 @@
         <v-spacer></v-spacer>
         <v-btn cols=1 text @click="$router.push({name: 'Training'})">실습</v-btn>
         <v-spacer></v-spacer>
-        <v-btn cols=1 text @click="$router.push({name: 'Login'})">로그인</v-btn>
       </v-row>
     </v-container>
   </v-app-bar>
