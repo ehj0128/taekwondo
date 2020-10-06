46920fe6c958b58c865c3ebc6587d21b431d7a09
Kanggyu.Min <macaron61@gmail.com>
2020-10-06 19:30:46 +0900

    [Feat(F)] 로고, 파비콘 변경

--- Front-end/src/components/Nav.vue
+++ Front-end/src/components/Nav.vue
@@ -3,7 +3,7 @@
     <v-container style="width: 63rem;">
       <v-row>
         <v-spacer></v-spacer>
-        <v-btn cols=1 text @click="$router.push({name: 'Home'})">로고</v-btn>
+        <v-btn cols=1 text @click="$router.push({name: 'Home'})"><v-img width="30" src="@/assets/nav_logo.png"></v-img></v-btn>
         <v-spacer></v-spacer>
         <v-btn cols=1 text @click="$router.push({name: 'Introduction'})">태권도란</v-btn>
         <v-spacer></v-spacer>
